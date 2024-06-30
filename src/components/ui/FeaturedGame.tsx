31cc8c78623f842a14248201ce58f2169808bd47
thor-tor <michal.kabara@gmail.com>
2024-06-30 21:35:43 +0200

    adds links to featured game component

--- src/components/ui/FeaturedGame.tsx
+++ src/components/ui/FeaturedGame.tsx
@@ -3,6 +3,7 @@ import { FaRegCalendarAlt } from "react-icons/fa";
 import { PiClockCountdownBold } from "react-icons/pi";
 import { useFetchFeaturedGame } from "../../customHooks/useFetchFeaturedGame";
 import { DateDisplay } from "./DateDisplay";
+import { Link } from "react-router-dom";
 
 export const FeaturedGame = () => {
   const { isPending, error, data } = useFetchFeaturedGame();
@@ -16,18 +17,24 @@ export const FeaturedGame = () => {
     <div className="bg-zinc-200 dark:bg-zinc-800 rounded-lg p-5 text-zinc-700 dark:text-white flex flex-col items-center gap-5">
       <h3 className="font-bold">Wyróżniony mecz</h3>
       <div className="flex flex-row gap-6 items-start">
-        <div className="flex flex-col text-sm items-center gap-2 w-1/2">
-          <img src={data.homeTeam?.logoUrl} alt="" className="size-16 rounded-md p-1 bg-white" />
-          <p className="text-xs text-center">{data.homeTeam?.name}</p>
-        </div>
+        <Link to={`/team/${data.homeTeam?.id}`} className="w-1/2">
+          <div className="flex flex-col text-sm items-center gap-2 ">
+            <img src={data.homeTeam?.logoUrl} alt="" className="size-16 rounded-md p-1 bg-white" />
+            <p className="text-xs text-center">{data.homeTeam?.name}</p>
+          </div>
+        </Link>
 
-        <span className="h-full flex items-center text-xs text-center whitespace-nowrap">
-          {data.homeGoals} : {data.awayGoals}
+        <span className="h-full flex items-center text-md font-bold text-center whitespace-nowrap">
+          <Link to={`/game/${data.id}`}>
+            {data.homeGoals} : {data.awayGoals}
+          </Link>
         </span>
-        <div className="flex flex-col text-sm items-center gap-2 w-1/2">
-          <img src={data.awayTeam?.logoUrl} alt="" className="size-16 rounded-md p-1 bg-white" />
-          <p className="text-xs text-center">{data.awayTeam?.name}</p>
-        </div>
+        <Link to={`/team/${data.awayTeam?.id}`} className="w-1/2">
+          <div className="flex flex-col text-sm items-center gap-2">
+            <img src={data.awayTeam?.logoUrl} alt="" className="size-16 rounded-md p-1 bg-white" />
+            <p className="text-xs text-center">{data.awayTeam?.name}</p>
+          </div>
+        </Link>
       </div>
       <div className="flex flex-row gap-5 text-xs mt-2">
         <div className="flex flex-row gap-2 items-center">
@@ -40,7 +47,7 @@ export const FeaturedGame = () => {
         </div>
         <div className="flex flex-row gap-2 items-center">
           <PiClockCountdownBold className="size-5" />
-          02:02:12
+          {data.isFinished ? "Zakończony" : null}
         </div>
       </div>
     </div>
