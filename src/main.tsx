6fc59554852e9aa2cb8030a09efcb56d2e17a872
thor-tor <michal.kabara@gmail.com>
2024-07-01 10:46:30 +0200

    disabled refetch on windows focus in tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,13 @@ import { CombinedContext } from "./context/CombinedContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./router";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
