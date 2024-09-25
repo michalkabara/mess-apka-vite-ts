c7cabc6975361d9666c393fa9077db56f370a76c
thor-tor <michal.kabara@gmail.com>
2024-09-25 18:21:39 +0200

    added /api to more link

--- src/customHooks/fetchTeamData/useFetchTeamPlayers.ts
+++ src/customHooks/fetchTeamData/useFetchTeamPlayers.ts
@@ -5,6 +5,6 @@ import { Player } from "../../types";
 export const useFecthTeamPlayers = (teamId: string | undefined) => {
   return useQuery<Player[]>({
     queryKey: ["teamPlayersData", teamId],
-    queryFn: () => fetchData(`${import.meta.env.VITE_API_URL}/Teams/${teamId}/players`),
+    queryFn: () => fetchData(`${import.meta.env.VITE_API_URL}/api/Teams/${teamId}/players`),
   });
 };
